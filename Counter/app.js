9cbe1bcaa03567cead3f970b00ac30e155c3a27c
Aydan Samedova <aydan2903@gmail.com>
2021-01-09 00:33:09 +0400

    counter is ready

--- Counter/app.js
+++ Counter/app.js
@@ -1,18 +1,40 @@
-//variables
-const count = 0;
-const btns = document.querySelectorAll('.btn')
-
-const countText = document.querySelector('#text');
-
-
+//variable
+const upCount = document.getElementById('up');
+const downCount = document.getElementById('down');
+const resetCount = document.getElementById('reset');
+const count = document.getElementById('text');
 
 
-btns.forEach(function() {
-    btns.addEventListener("click", function() {
-        if ("up") {
-            count++;
-        } else if ("down") {
-            count--;
-        }
-    })
-})
+//------------functions-----------------------
+
+//1. Count ++
+function upBtn() {
+    count.innerText = Number(count.innerText) + 1
+    colorCount()
+}
+upCount.addEventListener("click", upBtn);
+
+//2. Count --
+function downBtn() {
+    count.innerText = Number(count.innerText) - 1
+    colorCount()
+}
+downCount.addEventListener("click", downBtn);
+
+//3.Count = 0
+function resetBtn() {
+    count.innerText = 0;
+    colorCount()
+}
+resetCount.addEventListener("click", resetBtn)
+
+//Function color 
+function colorCount() {
+    if (count.innerText == 0) {
+        count.style.color = 'black'
+    } else if (count.innerText > 0) {
+        count.style.color = 'rgb(223, 223, 64)'
+    } else {
+        count.style.color = 'rgba(240, 54, 54, 0.815)'
+    }
+}
